8af730a91d185dd351699894aa5c586551d306a6
Martin Gausby <martin@gausby.dk>
2015-12-13 23:15:58 +0100

    Redid the specs definitions for the public API
    I took some inspiration from Anton Fagerberg's `encodable` type in his
    elixir_bencode project.

--- lib/bencode.ex
+++ lib/bencode.ex
@@ -1,17 +1,19 @@
 defmodule Bencode do
-  @spec encode(any) :: String.t
+  @type encodable :: binary | atom | Map | List | Integer
+
+  @spec encode(encodable) :: binary | {:error, binary}
   defdelegate encode(data),
     to: Bencode.Encoder
 
-  @spec decode(String.t) :: {:ok, Integer | String.t | List | Map | no_return}
+  @spec decode(binary) :: encodable | {:error, binary}
   defdelegate decode(data),
     to: Bencode.Decoder
 
-  @spec decode!(String.t) :: Integer | String.t | List | Map | nil | no_return
+  @spec decode!(binary) :: encodable | {:error, binary}
   defdelegate decode!(data),
     to: Bencode.Decoder
 
-  @spec decode_with_info_hash(String.t) :: {:ok, Integer | String.t | List | Map | no_return, nil | binary}
+  @spec decode_with_info_hash(binary) :: {:ok, encodable, <<_::20 * 8>> | nil} | {:error, binary}
   defdelegate decode_with_info_hash(data),
     to: Bencode.Decoder
 end
